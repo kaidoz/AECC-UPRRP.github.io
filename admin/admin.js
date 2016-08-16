94d2a1d5aad1256f4e3e1faa938dbdcef837eeda
Jeffrey Chan <jachan.santiago@gmail.com>
2016-08-16 18:19:00 -0400

    adding admin -> add member component alpha

---
+++ admin/admin.js
@@ -0,0 +1,49 @@
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC3UWKkBeQCRo2YaL7pdek_R2UJuyoDvfk",
+    authDomain: "aecc-uprrp.firebaseapp.com",
+    databaseURL: "https://aecc-uprrp.firebaseio.com",
+    storageBucket: "aecc-uprrp.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+
+var app = angular.module('adminApp', [ 'firebase']);
+
+app.controller('addMemberCtrl', addMemberController);
+
+
+function addMemberController ($scope, $firebaseObject) {
+    
+    $scope.name;
+    $scope.email;
+    $scope.facebook;
+    $scope.profilePic;
+    
+    $scope.addMember = function (){
+        
+        var membersRef = firebase.database().ref('members');
+        membersRef.push({
+            name: $scope.name,
+            email: $scope.email,
+            facebookProfile: $scope.facebook,
+            profilePic: $scope.profilePic
+        }).then(function(){
+            alert("Memeber Added successful");
+            $scope.name = '';
+            $scope.email = '';
+            $scope.facebook = '';
+            $scope.profilePic = '';
+            
+        }).catch(function(error){
+            alert(error.message);
+        })
+    }
+    
+    
+}
+
+    
